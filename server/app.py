56c00812b75a424e0762297629855a471209a464
shadrack-star <shaddkiplangat98@gmail.com>
2024-07-15 11:08:03 +0300

    done

--- server/app.py
+++ server/app.py
@@ -44,9 +44,23 @@ api.add_resource(Plants, '/plants')
 class PlantByID(Resource):
 
     def get(self, id):
-        plant = Plant.query.filter_by(id=id).first().to_dict()
+        plant = Plant.query.filter_by(id = id).first().to_dict()
         return make_response(jsonify(plant), 200)
+    
+    def patch(self, id):
+        plant = Plant.query.filter_by(id = id).first()
+        data = request.get_json()
+        for key in data:
+            setattr(plant, key, data[key])
+        db.session.add(plant)
+        db.session.commit()
+        return plant.to_dict(), 202
 
+    def delete(self, id):
+        plant = Plant.query.filter_by(id = id).first()
+        db.session.delete(plant)
+        db.session.commit()
+        return {},204
 
 api.add_resource(PlantByID, '/plants/<int:id>')
 
